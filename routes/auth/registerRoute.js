dee145313c5ecf44ffe7fea3b64be6e3b19f4de0
Davin Wijaya <wijayadavin@gmail.com>
2020-11-12 21:08:06 +0700

    commit await

--- routes/auth/registerRoute.js
+++ routes/auth/registerRoute.js
@@ -5,44 +5,42 @@ const { salt } = require('../../helper/bcryptHelper');
 const routeErrorHandler = require('../../middleware/errorHandler');
 const dayjs = require('dayjs')
 const regexHelper = require('../../helper/regexHelper')
-const {validateDateFormat} = require('../../helper/dateHelper')
+const { validateDateFormat } = require('../../helper/dateHelper')
 
 
-app.post('/auth/register', (req, res, next) => {
+app.post('/auth/register', async (req, res, next) => {
   // Check credential's format:
   const username = req.body.username
   const email = req.body.email
   const password = req.body.password
   const anyWrongCredentialFormat = (
-    !regexHelper.username.test(username) || 
-    !regexHelper.email.test(email) || 
+    !regexHelper.username.test(username) ||
+    !regexHelper.email.test(email) ||
     !regexHelper.password.test(password)
   )
   if (anyWrongCredentialFormat) {
-    return next (new Error('ERR_INVALID_FORMAT'))
+    return next(new Error('ERR_INVALID_FORMAT'))
   };
 
+  // Check duplicate username or email:
+  // const isFoundDuplicateUsername = await db.get('users', username)
+  // if(isFoundDuplicateUsername) {
+  //   return next (new Error('409'))
+  // }
+
   // Check birth date's format:
-  if(!validateDateFormat(req.body.birthDate, 'YYYY-MM-DD')) {
-    next (new Error('ERR_INVALID_FORMAT'))
+  if (!(await validateDateFormat(req.body.birthDate, 'YYYY-MM-DD'))) {
+    return next(new Error('ERR_INVALID_FORMAT'))
   }
 
-  salt(password)
-    .then(hashedPassword => {
-      req.body.password = hashedPassword
-      return db.add('users', req.body)
-      }
-    )
-    .then(addUserResult => {
-      if (addUserResult) {
-        return res.send(addUserResult)
-      } else {
-        next (new Error('ERR_BAD_FIELD_ERROR'))
-      }
-    })
-    .catch(err => {
-      next(err)
-    })
+  const hashedPassword = await salt(password)
+  req.body.password = hashedPassword
+  result = await db.add('users', req.body)
+  if (result) {
+    return res.send(result)
+  } else {
+    next(new Error('ERR_BAD_FIELD_ERROR'))
+  }
 })
 
 
