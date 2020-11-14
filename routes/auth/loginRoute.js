83d29be8d537bb3cb0c6240558bd497ff3e2832c
Davin Wijaya <wijayadavin@gmail.com>
2020-11-14 09:59:22 +0700

    merge branch login

--- routes/auth/loginRoute.js
+++ routes/auth/loginRoute.js
@@ -4,36 +4,61 @@ const db = require('../../controller/dbController')
 const jwt = require('jsonwebtoken')
 const routeErrorHandler = require('../../middleware/errorHandler')
 const { checkPassword } = require('../../helper/bcryptHelper')
-const secret = process.env.JWT_SECRET
+const jwtConfig = require('../../configs/jwtConfig')
 
-app.post('/auth/login', (req, res, next) => {
-  const username = req.body.username
-  const password = req.body.password
-  let user;
+app.post('/auth/login', async (req, res, next) => {
+  const body = req.body
+  const { email, username, password } = body
 
-  db.get('users', { username })
-    .then(userSearchResults => {
-      if (userSearchResults.length) {
-        user = userSearchResults[0]
-        return checkPassword(password, user.password)
-      } else {
-        res.status(401).send('Unauthorized')
-      }
-    })
-    .then(isPasswordMatch => {
+  // mengecek agar struktur request body harus sama dengan 2.
+  if (Object.keys(body).length != 2) return res.status(400).send('body not allowed')
+
+  // untuk mengecek key password sudah benar atau belum
+  if (!body.password) return res.status(400).send('bad request, please write the right password key')
+
+  let user
+  if (body.hasOwnProperty("username")) {
+    let userSearchResult = await db.get('users', { username })
+      .catch(err => next(err))
+
+    if (userSearchResult.length) {
+      user = userSearchResult[0]
+      let isPasswordMatch = await checkPassword(password, user.password)
+        .catch(err => next(err))
+
       if (isPasswordMatch) {
-        const token = jwt.sign(user, secret, {
-          expiresIn: '6h'
-        })
+        const token = jwt.sign({ id: user.id }, jwtConfig.secret, jwtConfig.options)
         user.token = token
-        res.send(user)
+        res.send({ token })
       } else {
-        res.status(401).send('Unauthorized')
+        res.status(401).send('please input the right password')
       }
-    })
-    .catch((err) => {
-      next(err)
-    })
+    }
+    else {
+      res.status(404).send('username is not found')
+    }
+  } else if (body.hasOwnProperty("email")) {
+    let userSearchResult = await db.get('users', { email })
+      .catch(err => next(err))
+
+    if (userSearchResult.length) {
+      user = userSearchResult[0]
+      let isPasswordMatch = await checkPassword(password, user.password)
+        .catch(err => next(err))
+
+      if (isPasswordMatch) {
+        const token = jwt.sign({ id: user.id }, jwtConfig.secret, jwtConfig.options)
+        user.token = token
+        res.send({ token })
+      } else {
+        res.status(401).send('Please input the right password')
+      }
+    } else {
+      res.status(404).send('email is not found')
+    }
+  } else {
+    res.status(400).send('bad request, please write the right key username or email')
+  }
 })
 
 app.use(routeErrorHandler)
