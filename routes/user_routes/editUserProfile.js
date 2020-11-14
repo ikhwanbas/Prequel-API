83365bc4be6aaaa38ce397179b352592b48e073a
Davin Wijaya <wijayadavin@gmail.com>
2020-11-14 08:32:31 +0700

    debug

--- routes/user_routes/editUserProfile.js
+++ routes/user_routes/editUserProfile.js
@@ -1,42 +1,47 @@
 const express = require('express')
-const app = express.Router()
+const router = express.Router()
 const db = require('../../controller/dbController')
 const routeErrorHandler = require('../../middleware/errorHandler')
+const auth = require('../../middleware/auth')
+const passport = require('passport')
 
-// // Browse users:
-// router.patch('/u/:username',
-//     userPermission(['user', 'admin']), (req, res) => {
-//         // Check param if found or not
-//         const foundUser = getData('users', { id: req.user.id }[0]);
-//         if (!foundUser || foundUser.length == 0) {
-//             return res.status(400).send('Bad request');
-//         }
+// Browse users:
+router.patch('/u/:username/edit',
+    auth.authenticate('bearer', { session: true }),
+    (req, res) => {
+        console.log(req.session.passport.user)
 
-//         // Check if requested profile to edit is matched with username from token:
-//         if (req.params.username != foundUser) {
-//             return res.status(401).send('Unauthorized');
-//         };
+        // Check if requested profile to edit is matched with username from token:
+        if (req.params.username != '') {
+            return res.status(401).send('Unauthorized');
+        };
 
-//         // Search if username is existed in database:
-//         const isUsernameExist = getData('users', { username: req.body.username });
-//         if (isUsernameExist & isUsernameExist.length > 1) {
-//             return res.status(400).send('Username is existed');
-//         }
+        // Search if the new username is existed in database:
+        const isUsernameExist = getData('users', { username: req.body.username });
+        if (isUsernameExist & isUsernameExist.length > 1) {
+            return res.status(400).send('Username is existed');
+        }
 
-//         // if passed, users can edit their username:
-//         req.body.id = req.user.id;
-//         const result = editData('users', req.body.id, req.body);
+        // Search if the new email is existed in database:
+        const isEmailExist = getData('users', { username: req.body.username });
+        if (isEmailExist & isEmailExist.length > 1) {
+            return res.status(400).send('Username is existed');
+        }
 
-//         if (!result) {
-//             // if false
-//             res.status(400).send('Bad request');
-//         } else {
-//             // if true
-//             res.send(result);
-//         }
-//         return;
-//     });
+        // if passed, the user can edit his/her data:
+        req.body.id = req.user.id;
+        const result = editData('users', req.body.id, req.body);
 
-app.use(routeErrorHandler)
+        if (!result) {
+            // if false
+            res.status(400).send('Bad request');
+        } else {
+            // if true
+            res.send(result);
+        }
+        return;
+    });
 
-module.exports = app
+router.use(routeErrorHandler)
+
+module.exports = router
