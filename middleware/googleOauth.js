3671a569dd42e8a4914b6a116244aaf7b307aafd
Davin Wijaya <wijayadavin@gmail.com>
2020-11-17 10:03:04 +0700

    update google oauth credentials

--- middleware/googleOauth.js
+++ middleware/googleOauth.js
@@ -15,8 +15,8 @@ passport.deserializeUser(function (user, done) {
 //   credentials (in this case, an accessToken, refreshToken, and Google
 //   profile), and invoke a callback with a user object.
 passport.use(new GoogleStrategy({
-    clientID: '516244174897-tqbj9qhkomvlij837fat694s7mb24ljv.apps.googleusercontent.com',
-    clientSecret: 'tThXCOEo51I35Al9K28G3f6k',
+    clientID: '516244174897-fja16kocv5rtf9luqjp50p7bbhqsicp7.apps.googleusercontent.com',
+    clientSecret: 'vWQmLRPNDbF9_8bxTK0brkx-',
     callbackURL: "http://ec2-3-1-218-225.ap-southeast-1.compute.amazonaws.com/auth/google/callback"
 },
     (accessToken, refreshToken, profile, done) => {
