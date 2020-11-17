1a13aa032ad67999ff8bc19b57dc1bc8bbfdc913
Davin Wijaya <wijayadavin@gmail.com>
2020-11-17 09:45:55 +0700

    update url google o auth

--- middleware/googleOauth.js
+++ middleware/googleOauth.js
@@ -17,7 +17,7 @@ passport.deserializeUser(function (user, done) {
 passport.use(new GoogleStrategy({
     clientID: '516244174897-tqbj9qhkomvlij837fat694s7mb24ljv.apps.googleusercontent.com',
     clientSecret: 'tThXCOEo51I35Al9K28G3f6k',
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "http://ec2-3-1-218-225.ap-southeast-1.compute.amazonaws.com/auth/google/callback"
 },
     (accessToken, refreshToken, profile, done) => {
         // use the profile info (mainly profile id) to check if the user is registered in database
