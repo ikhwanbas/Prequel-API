d9878ae607e737e86ef771a3f1ec2bec9275290a
Davin Wijaya <wijayadavin@gmail.com>
2020-11-19 05:47:01 +0700

    space

--- controller/movieController.js
+++ controller/movieController.js
@@ -24,66 +24,65 @@ function getMoviebyGenre(
   limit
 ) {
   let query = `
-SELECT m.id,
-m.title,
+SELECT m.id, 
+m.title, 
 
-GROUP_CONCAT(
-CASE WHEN mi.type = '/poster'
-THEN mi.image_url ELSE NULL END SEPARATOR ', '
-) as poster_url,
+GROUP_CONCAT( 
+CASE WHEN mi.type = '/poster' 
+THEN mi.image_url ELSE NULL END SEPARATOR ', ' 
+) as poster_url, 
 
 GROUP_CONCAT(
 CASE WHEN mi.type != '/poster'
-THEN mi.image_url ELSE NULL END SEPARATOR ', '
-) as image_urls,
+THEN mi.image_url ELSE NULL END SEPARATOR ', ' 
+) as image_urls, 
 
-m.release_date,
-m.synopsis,
-m.info,
-m.trailer_url,
-COUNT(ml.id) as like_count,
-AVG(mr.rating) as average_rating,
-COUNT(mr.id) as review_count,
+m.release_date, 
+m.synopsis, 
+m.info, 
+m.trailer_url, 
+COUNT(ml.id) as like_count, 
+AVG(mr.rating) as average_rating, 
+COUNT(mr.id) as review_count, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/genre'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as genre,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/genre' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as genre, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/star'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as star,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/star' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as star, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/production'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as production,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/production' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as production, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/director'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as director
+GROUP_CONCAT( 
+CASE WHEN md.type = '/director' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as director 
 
-FROM movies m
+FROM movies m 
 
-LEFT JOIN movie_details md
-ON m.id = md.movie_id
+LEFT JOIN movie_details md 
+ON m.id = md.movie_id 
 
-LEFT JOIN movie_images mi
-ON m.id = mi.movie_id
+LEFT JOIN movie_images mi 
+ON m.id = mi.movie_id 
 
-LEFT JOIN movie_reviews mr
-ON m.id = mr.movie_id
+LEFT JOIN movie_reviews mr 
+ON m.id = mr.movie_id 
+ 
+LEFT JOIN movie_likes ml 
+ON m.id = ml.movie_id 
 
-LEFT JOIN movie_likes ml
-ON m.id = ml.movie_id
+GROUP BY m.id 
 
-GROUP BY m.id
-
-HAVING genre LIKE "%${genre}%"
- LIMIT ${startIndex}, ${limit}
-`
+HAVING genre LIKE "%${genre}%" 
+ LIMIT ${startIndex}, ${limit};`
 
   return new Promise((resolve, reject) => {
     db.query(query, (err, result) => {
@@ -105,70 +104,68 @@ HAVING genre LIKE "%${genre}%"
 
 function search(searchParameter, startIndex, limit) {
   let query = `SELECT 
-SELECT m.id,
-m.title,
-GROUP_CONCAT(
-CASE WHEN mi.type = '/poster'
-THEN mi.image_url ELSE NULL END SEPARATOR ', '
-) as poster_url,
+SELECT m.id, 
+m.title, 
+GROUP_CONCAT( 
+CASE WHEN mi.type = '/poster' 
+THEN mi.image_url ELSE NULL END SEPARATOR ', ' 
+) as poster_url, 
 
-GROUP_CONCAT(
-CASE WHEN mi.type != '/poster'
-THEN mi.image_url ELSE NULL END SEPARATOR ', '
-) as image_urls,
+GROUP_CONCAT( 
+CASE WHEN mi.type != '/poster' 
+THEN mi.image_url ELSE NULL END SEPARATOR ', ' 
+) as image_urls, 
 
-m.release_date,
-m.synopsis,
-m.info,
-m.trailer_url,
-COUNT(ml.id) as like_count,
-AVG(mr.rating) as average_rating,
-COUNT(mr.id) as review_count,
+m.release_date, 
+m.synopsis, 
+m.info, 
+m.trailer_url, 
+COUNT(ml.id) as like_count, 
+AVG(mr.rating) as average_rating, 
+COUNT(mr.id) as review_count, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/genre'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as genre,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/genre' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as genre, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/star'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as star,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/star' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as star, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/production'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as production,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/production' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as production, 
 
-GROUP_CONCAT(
-CASE WHEN md.type = '/director'
-THEN md.text ELSE NULL END SEPARATOR ', '
-) as director,
+GROUP_CONCAT( 
+CASE WHEN md.type = '/director' 
+THEN md.text ELSE NULL END SEPARATOR ', ' 
+) as director, 
+GROUP_CONCAT(movie_details.text SEPARATOR ', ') as details 
 
-GROUP_CONCAT(movie_details.text SEPARATOR ', ') as details
+FROM movies m 
 
-FROM movies m
+LEFT JOIN movie_details md 
+ON m.id = md.movie_id 
 
-LEFT JOIN movie_details md
-ON m.id = md.movie_id
+LEFT JOIN movie_images mi 
+ON m.id = mi.movie_id 
 
-LEFT JOIN movie_images mi
-ON m.id = mi.movie_id
+LEFT JOIN movie_reviews mr 
+ON m.id = mr.movie_id 
 
-LEFT JOIN movie_reviews mr
-ON m.id = mr.movie_id
+LEFT JOIN movie_likes ml 
+ON m.id = ml.movie_id 
 
-LEFT JOIN movie_likes ml
-ON m.id = ml.movie_id
-
-GROUP BY m.id
+GROUP BY m.id 
 
 HAVING title LIKE '%${searchParameter}%' 
 OR synopsis LIKE '%${searchParameter}%' 
 OR info LIKE '%${searchParameter}%' 
-OR details LIKE '%${searchParameter}%'
-LIMIT ${startIndex}, ${limit}
-`
+OR details LIKE '%${searchParameter}%' 
+LIMIT ${startIndex}, ${limit};`
 
   return new Promise((resolve, reject) => {
     db.query(query, (err, result) => {
@@ -243,7 +240,7 @@ LEFT JOIN movie_likes ml
 ON m.id = ml.movie_id
 
 GROUP BY m.id
-LIMIT ${startIndex}, ${limit}`
+LIMIT ${startIndex}, ${limit};`
 
 
   return new Promise((resolve, reject) => {
