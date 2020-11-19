72229b25d1b8708e784bda70d89e1e63b8b43052
Davin Wijaya <wijayadavin@gmail.com>
2020-11-19 21:33:15 +0700

    send error

--- routes/admin/readEntity.js
+++ routes/admin/readEntity.js
@@ -9,14 +9,23 @@ const pluralize = require('pluralize')
 router.get('/admin/:tableName', async (req, res, next) => {
     // Check request:
     const tableName = pluralize.plural(req.params.tableName)
-    if (tableName.length <= 0 ||
-        req.body <= 0) {
+    if (tableName.length <= 0) {
         return res.status(400).send('Bad request')
+    }
+
+    let page = 0
+    if (req.query.page) {
+        page = req.query.page
+    }
+
+    let limit = 8
+    if (req.query.limit) {
+        limit = req.query.limit
     }
 
     // Try adding the data into the database
     try {
-        const result = await db.get(tableName)
+        const result = await db.getPage(tableName, {}, '*', page, limit)
         return res.status(200).send(result)
     } catch (err) {
         console.log(err);
