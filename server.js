978880cc966113694bf07b59ab02bf34dc956c9b
Davin Wijaya <wijayadavin@gmail.com>
2020-11-21 20:18:37 +0700

    fix cors

--- server.js
+++ server.js
@@ -20,6 +20,19 @@ app.use(cookieSession({
   keys: ['key1, key2']
 }))
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  )
+  if (req.method === 'OPTIONS') {
+    res.header('Acess-Control-Allow-Methods',
+      'POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 const corsOptionsDelegate = function (req, callback) {
   let corsOptions;
